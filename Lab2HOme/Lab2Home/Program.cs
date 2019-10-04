9a2e9b03fcd9d1319e696ca1de51d135d128db69
Richard <r.mora@students.clark.edu>
2019-10-03 17:16:38 -0700

    Lab 2 from home complete

---
+++ Lab2HOme/Lab2Home/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2Home
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Let a look here to see what we see below");
+            Console.WriteLine();
+
+            SportsCar myCar = new SportsCar();
+            Console.WriteLine("Here comes the {0}", myCar.GetPetName());
+        }
+    }
+}
---
+++ Lab2HOme/Lab2Home/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab2Home
+{
+    public class SportsCar : Car
+    {
+        public string GetPetName()
+        {
+            PetName = "sum";
+            return PetName; 
+        }
+    }
+}
